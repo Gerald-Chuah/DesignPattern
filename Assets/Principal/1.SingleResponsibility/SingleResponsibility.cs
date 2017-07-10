0264ebb91c0a0490d71889615c12387ec2a5be89
Gerald-Chuah <geraldchuah94@gmail.com>
2017-07-10 15:27:33 +0800

    v0.01 [Add] 1. Single Responsibility -Complete 2.Open Close -In Progress

---
+++ Assets/Principal/1.SingleResponsibility/SingleResponsibility.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Journal
+{
+	private readonly List<string> entries = new List<string>();
+	private static int count=0;
+	public int AddEntry(string text)
+	{
+		entries.Add(String.Format("{0}:{1}",++count,text));
+		// entries.Add($"{++count}: {text}"); Only C# 6.0 or greater support interpolated strings
+		return count;
+	}
+
+	public void RemoveEntry(int index)
+	{
+		entries.RemoveAt(index);
+	}
+
+
+	public override string ToString()
+	{
+		return string.Join(Environment.NewLine,entries.ToArray());
+	}
+}
+
+public class Persistence
+{
+	public void SaveToFile(Journal journal, string filename, bool overwrite=false)
+	{
+		if (overwrite || !File.Exists(filename))
+		{
+			File.WriteAllText(filename,journal.ToString());
+		}
+	}
+}
+
+public class SingleResponsibility : MonoBehaviour
+{
+
+	void Start () 
+	{
+		var j = new Journal();
+		j.AddEntry("Hello");
+		j.AddEntry("Wei");
+		UnityEngine.Debug.Log(j);
+
+		var p = new Persistence ();
+		var filename = Application.dataPath + "/TextFile/Journal.txt";
+		p.SaveToFile(j, filename,true);
+		Process.Start(filename);//open the txt file OwO
+
+	}
+
+}
---
+++ Assets/Principal/2.Open-Closed/OpenClosed.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq.Expressions;
+
+
+// to get variable name
+public static class MemberInfoGetting
+{
+	public static string GetMemberName<T>(Expression<Func<T>> memberExpression)
+	{
+		MemberExpression expressionBody = (MemberExpression)memberExpression.Body;
+		return expressionBody.Member.Name;
+	}
+}
+
+public enum Color
+{
+	Red,Green,Blue
+}
+
+public enum Size
+{
+	Small,Medium,Big
+}
+
+public class Product
+{
+	public string m_name;
+	public Color m_color;
+	public Size m_size;
+
+	public Product (string name,Color color,Size size)
+	{
+		if (name == null)
+		{
+			throw new ArgumentNullException (paramName: MemberInfoGetting.GetMemberName(()=>name));
+			//throw new ArgumentNullException (paramName: nameof(name); =>After C# 6.0
+
+		}
+			
+		m_name = name;
+		m_color = color;
+		m_size = size;
+	}
+}
+
+public class ProductFilter
+{
+	public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+	{
+		foreach (var p in products)
+		{
+			if (p.m_size == size)
+				yield return p;
+		}
+	}
+	public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+	{
+		foreach (var p in products)
+		{
+			if (p.m_color == color)
+				yield return p;
+		}
+	}
+
+	public IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products, Size size,Color color)
+	{
+		foreach (var p in products)
+		{
+			if (p.m_size == size && p.m_color == color)
+				yield return p;
+		}
+	}
+
+}
+
+public class OpenClosed : MonoBehaviour 
+{
+	
+
+	void Start () 
+	{
+
+		var apple = new Product ("Apple",Color.Red,Size.Small);
+		var tomato = new Product ("Tomato",Color.Red,Size.Medium);
+		var grass = new Product ("Grass",Color.Green,Size.Medium);
+
+		Product[] products = {apple,tomato,grass};
+
+		var pf = new ProductFilter ();
+		Debug.Log("Old Method");
+
+		foreach (var p in pf.FilterByColor(products,Color.Red))
+		{
+			string text = string.Format("{0} is red color.",p.m_name);
+			Debug.Log(text);
+		}
+
+	}
+	
+
+}
