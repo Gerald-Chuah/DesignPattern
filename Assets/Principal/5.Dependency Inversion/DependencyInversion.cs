ebebebb68028d645352bce10d0d2c662d6a0d2ae
Gerald-Chuah <geraldchuah94@gmail.com>
2017-07-11 15:23:33 +0800

    v0.04 [Add] Dependency Inversion.

--- Assets/Principal/5.Dependency Inversion/DependencyInversion.cs
+++ Assets/Principal/5.Dependency Inversion/DependencyInversion.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 using System;
 using System.Linq;
 
+public class RelationshipClass
+{
+	public Person Person1;
+	public RelationshipType Relation;
+	public Person Person2;
+
+	public RelationshipClass()
+	{
+		
+	}
+
+    public RelationshipClass(Person person1, RelationshipType relation, Person person2)
+    {
+        Person1 = person1;
+        Relation = relation;
+        Person2 = person2;
+    }
+    
+}
+
+
 public enum RelationshipType
 {
 	Parent,
-	child,
+	Child,
 	Sibling
 }
 
 public class Person
 {
-	public string name;
+	public string Name;
+
 }
 
 public interface IRelationshipBrowser
@@ -21,28 +43,61 @@ public interface IRelationshipBrowser
 	IEnumerable<Person> FindAllChildrenOf(string name);
 }
 
-public class RelationShip//low level
+public class RelationShip: IRelationshipBrowser//low level
 {
-	private List<Tuple<Person,RelationshipType,Person>> relations 
-	= new List<Tuple<Person, RelationshipType, Person>>();
+    //private List<Tuple<Person, RelationshipType, Person>> abc
+    //= new List<Tuple<Person, RelationshipType, Person>>();
+
+    //	public void AddParentAndChild(Person parent, Person child)
+    //	{
+    //		relations.Add(new Tuple<Person,RelationshipType,Person>(parent,RelationshipType.Parent,child));
+    //		relations.Add(new Tuple<Person,RelationshipType,Person>(child, RelationshipType.child,parent));
+    //	}
+
+    //	public List<Tuple<Person,RelationshipType,Person>> Relations()
+    //	{
+    //		return relations;
+    //	}
+
+    private readonly List<RelationshipClass> _relations = new List<RelationshipClass>();
+
+
 
 	public void AddParentAndChild(Person parent, Person child)
 	{
-		relations.Add();
-		relations.Add(child, RelationshipType.child, parent);
+		_relations.Add(new RelationshipClass(parent,RelationshipType.Parent,child));
+		_relations.Add(new RelationshipClass(child,RelationshipType.Child,parent));
 	}
 
-	public List<Tuple<Person,RelationshipType,Person>> Relations()
+	public List<RelationshipClass> Relations()
 	{
-		return relations;
+	       return _relations;
 	}
+
 
 	public IEnumerable<Person> FindAllChildrenOf(string name)
 	{
-		return relations.Where
-			(x => x.Item1 == name &&
-				x.Item2 == RelationshipType.Parent).Select(r =>r.Item3);
+		return _relations.Where
+			(x => x.Person1.Name == name &&
+				x.Relation == RelationshipType.Parent).Select(r =>r.Person2);
 	}
+
+
+}
+
+
+public class Research
+{
+
+
+    public Research(IRelationshipBrowser browser, string name)
+    {
+        foreach (var p in browser.FindAllChildrenOf(name))
+        {
+            Debug.Log(string.Format("{0} has a child call {1}",name,p.Name));
+        }
+
+    }
 }
 
 public class DependencyInversion : MonoBehaviour 
@@ -51,8 +106,17 @@ public class DependencyInversion : MonoBehaviour
 
 	void Start ()
 	{
-		
-	}
+	    var parent = new Person {Name = "John"};
+	    var child1 = new Person { Name = "Lala"};
+	    var child2 = new Person { Name = "lili"};
+
+        var relationships= new RelationShip();
+	    
+        relationships.AddParentAndChild(parent,child1);
+        relationships.AddParentAndChild(parent,child2);
+
+        new Research(relationships,"John");
+    }
 	
 
 }
