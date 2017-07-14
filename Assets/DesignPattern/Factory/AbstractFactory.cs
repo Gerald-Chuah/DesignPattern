c4b4d60366a334f9cc7f983f8d43cd20ee3a751b
Gerald-Chuah <geraldchuah94@gmail.com>
2017-07-14 17:58:47 +0800

    v0.0.8 minor update

--- Assets/DesignPattern/Factory/AbstractFactory.cs
+++ Assets/DesignPattern/Factory/AbstractFactory.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
+using UnityEditor;
 using UnityEngine;
 
 
@@ -42,13 +45,25 @@ internal class TeaFactory : IHotDrinkFactory
     }
 }
 
-internal class CofeeFactory : IHotDrinkFactory
+internal class CoffeeFactory : IHotDrinkFactory
 {
 
     public IHotDrink PrepareDrink(int ml)
     {
         Debug.Log(string.Format("Starbuck, pour {0} ml", ml));
         return new Coffee();
+    }
+}
+
+class Factories
+{
+    public string name;
+    public IHotDrinkFactory factory;
+
+    public Factories(string name, IHotDrinkFactory factory)
+    {
+        this.name = name;
+        this.factory = factory;
     }
 }
 
@@ -60,20 +75,51 @@ public class HotDrinkMachine
         Coffee,Tea
     }
 
-    private  Dictionary<AvailableDrink,IHotDrinkFactory> factories = new Dictionary<AvailableDrink, IHotDrinkFactory>();
+    //private  Dictionary<AvailableDrink,IHotDrinkFactory> factories = new Dictionary<AvailableDrink, IHotDrinkFactory>();
+
+        private List<Factories> namedFactories= new List<Factories>();
 
     public HotDrinkMachine()
     {
-        foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
-        {
-            var factory = (IHotDrinkFactory)Activator.CreateInstance(Type.GetType(Enum.GetName(typeof(AvailableDrink), drink) + "Factory"));
-           factories.Add(drink, factory);
-        }
+        //foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+        //{
+        //    var factory = (IHotDrinkFactory)Activator.CreateInstance(Type.GetType(Enum.GetName(typeof(AvailableDrink), drink) + "Factory"));
+        //   factories.Add(drink, factory);
+        //}
+
+
+        //foreach (var type in typeof(HotDrinkMachine).Assembly.GetTypes())
+
+
+        Assembly assembly = typeof(HotDrinkMachine).Assembly;
+        Debug.Log(assembly);
+         foreach (var type in assembly.GetTypes())
+            {
+            Debug.Log(type);
+            if (typeof(IHotDrinkFactory).IsAssignableFrom(type) && !type.IsInterface)
+            {
+                string name = type.Name.Replace("Factory", String.Empty);
+                
+                IHotDrinkFactory factory = (IHotDrinkFactory) Activator.CreateInstance(type); 
+                namedFactories.Add(new Factories(name,factory));
+               
+            }
+
+
+            }
+
     }
 
     public IHotDrink MakeDrink(AvailableDrink drink, int amount)
     {
-         return factories[drink].PrepareDrink(amount);
+        //return factories[drink].PrepareDrink(amount);
+
+        return namedFactories.
+                            Find(x => x.name == Enum.GetName(typeof(AvailableDrink), (int) drink))
+                            .factory.PrepareDrink(amount);
+
+
+
     }
 
 }
@@ -89,8 +135,14 @@ public class AbstractFactory : MonoBehaviour
 		HotDrinkMachine machine= new HotDrinkMachine();
 
         IHotDrink drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 120);
+        IHotDrink drink2 = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Coffee, 500);
 
-         drink.Consume();
+        drink.Consume();
+
+        drink2.Consume();
+
+        Debug.Log(Enum.GetName(typeof(HotDrinkMachine.AvailableDrink), 0));
+        Debug.Log(Enum.GetName(typeof(HotDrinkMachine.AvailableDrink), 1));
     }
 
 }
---
+++ Assets/Other/DynamicScale.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DynamicScale : MonoBehaviour
+{
+    private Vector3 lastPosition;
+
+	void Start ()
+	{
+	    lastPosition = transform.position;
+	}
+	
+	
+	void LateUpdate ()
+	{
+	    Vector3 delta = transform.position - lastPosition;
+	    transform.localRotation = Quaternion.LookRotation(delta + Vector3.forward * 0.001f);
+	    float l = 1f + delta.magnitude;
+	    float wh = Mathf.Sqrt(1f / 1);
+        transform.localScale = new Vector3(wh,wh,l);
+	    lastPosition = transform.position;
+	}
+}
