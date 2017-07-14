6aa3b9f760ce98eef79789343bee5e9ffa1301e4
Gerald-Chuah <geraldchuah94@gmail.com>
2017-07-14 10:08:55 +0800

    v0.0.7 Factory design

---
+++ Assets/DesignPattern/Factory/AbstractFactory.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public interface IHotDrink
+{
+    void Consume();
+}
+
+internal class Tea : IHotDrink
+{
+
+    public void Consume()
+    {
+        Debug.Log("Nice tea OwO");
+    }
+}
+
+internal class Coffee : IHotDrink
+{
+
+    public void Consume()
+    {
+        Debug.Log("Kopi O");
+    }
+}
+
+public interface IHotDrinkFactory
+{
+    IHotDrink PrepareDrink(int ml);
+}
+
+internal class TeaFactory : IHotDrinkFactory
+{
+
+    public IHotDrink PrepareDrink(int ml)
+    {
+        Debug.Log(string.Format("TeaPot, pour {0} ml",ml));
+        return new Tea();
+    }
+}
+
+internal class CofeeFactory : IHotDrinkFactory
+{
+
+    public IHotDrink PrepareDrink(int ml)
+    {
+        Debug.Log(string.Format("Starbuck, pour {0} ml", ml));
+        return new Coffee();
+    }
+}
+
+public class HotDrinkMachine
+{
+    
+    public enum AvailableDrink
+    {
+        Coffee,Tea
+    }
+
+    private  Dictionary<AvailableDrink,IHotDrinkFactory> factories = new Dictionary<AvailableDrink, IHotDrinkFactory>();
+
+    public HotDrinkMachine()
+    {
+        foreach (AvailableDrink drink in Enum.GetValues(typeof(AvailableDrink)))
+        {
+            var factory = (IHotDrinkFactory)Activator.CreateInstance(Type.GetType(Enum.GetName(typeof(AvailableDrink), drink) + "Factory"));
+           factories.Add(drink, factory);
+        }
+    }
+
+    public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+    {
+         return factories[drink].PrepareDrink(amount);
+    }
+
+}
+
+
+
+public class AbstractFactory : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		HotDrinkMachine machine= new HotDrinkMachine();
+
+        IHotDrink drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 120);
+
+         drink.Consume();
+    }
+
+}
---
+++ Assets/DesignPattern/Factory/Factory.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Point
+{
+    private float x, y;
+
+    private Point(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("X: {0}, Y: {1}", x, y);
+    }
+
+
+    //Create new Point object one time (better)
+    public static Point OriginPoint = new Point(0, 0);
+
+
+    //Create new Point everytime when request
+    public static Point OriginPointFactory
+    {
+        get
+        {
+            return new Point(0, 0);
+        } 
+    }
+
+public static class Factory
+    {
+
+        public static Point CartesianPoint(float x, float y)
+        {
+            return new Point(x,y);
+        }
+
+        public static Point PolarPoint(float rho, float theta)
+        {
+            return new Point(rho * Mathf.Cos(theta), rho * Mathf.Sin(theta));
+        }
+    }
+}
+
+
+public class Factory : MonoBehaviour
+{
+
+	
+	void Start ()
+    {
+		Point cartesianPoint = Point.Factory.CartesianPoint(2,2);
+		Point polarPoint = Point.Factory.PolarPoint(2,2);
+
+        Point origin = Point.OriginPoint;
+        Point originFactory = Point.OriginPointFactory;
+
+        Debug.Log(string.Format("cartesianPoint: {0}", cartesianPoint));
+        Debug.Log(string.Format("polarPoint: {0}", polarPoint));
+        Debug.Log(string.Format("origin: {0}", origin));
+        Debug.Log(string.Format("originFactory: {0}", originFactory));
+    }
+	
+	
+}
